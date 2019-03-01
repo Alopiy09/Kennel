524a95ccb37bcfb3df495b67906347dceabc4c97
jacob sanders <jkehsanders@gmail.com>
2019-03-01 09:04:55 -0600

    Commiting to push needs more work

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import ReactDOM from 'react-dom';
-import React from 'react';
-import Kennel from "./components/kennel"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from "react-router-dom"
+import Kennel from './components/Kennel'
 
-ReactDOM.render(<Kennel />, document.querySelector("#root"));
+import './index.css'
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
-
-
-
+ReactDOM.render(
+    <Router>
+        <Kennel />
+    </Router>
+    , document.getElementById('root'))
